e6b5e6eecccea52ae2d747971a5ebb45c0513219
Manuel Sanchez del Rio <srio@esrf.eu>
2026-04-14 13:01:17 +0200

    released

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS2-XOPPY'
-VERSION = '1.0.12'
+VERSION = '1.0.13'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
@@ -36,8 +36,8 @@ CLASSIFIERS = [
 ]
 
 INSTALL_REQUIRES = (
-    'oasys2>=0.0.39',
-    'xoppylib>=1.0.53',
+    'oasys2>=0.0.42',
+    'xoppylib>=1.0.54',
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
